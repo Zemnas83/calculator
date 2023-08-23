9f5af2ec23832a198d3234852f17cb38e0e448cb
Zemnas83 <zayanmatin@gmail.com>
2023-08-23 17:23:22 -0400

    script functionality

--- script.js
+++ script.js
@@ -1,44 +1,104 @@
+let operator, num1 = '', num2 = '';
+let isOperatorChosen = false;
 
-let operator, num1, num2;
-let result;
+const resultDisplay = document.querySelector('.result'); // Assuming '.result' is where you display results
 
-const btn = document.querySelector('btn');
-btn.addEventListener('click',() =>{
-    alert()
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        let value = this.textContent;
+
+        if (!isNaN(value) || value === ".") { // Handle numbers and decimal
+            if (!isOperatorChosen) {
+                if (!(num1.includes('.') && value === '.')) { // Check to ensure only one decimal point
+                    num1 += value;
+                    resultDisplay.textContent = num1; // Display the number
+                }
+            } else {
+                if (!(num2.includes('.') && value === '.')) {
+                    num2 += value;
+                    resultDisplay.textContent = num2;
+                }
+            }
+        } else if (value === "CLEAR") { // Clear functionality
+            resetCalculator();
+        } else if (value === "+/-") { // Toggle between negative and positive
+            if (!isOperatorChosen) {
+                num1 = toggleSign(num1);
+                resultDisplay.textContent = num1;
+            } else {
+                num2 = toggleSign(num2);
+                resultDisplay.textContent = num2;
+            }
+        } else {
+            if (value === "=") {
+                const output = operate(operator, parseFloat(num1), parseFloat(num2));
+                resultDisplay.textContent = output; // Display the result
+
+                // Reset for next operation but retain the result
+                num1 = output.toString();
+                num2 = '';
+                operator = null;
+                isOperatorChosen = false;
+            } else {
+                if (!operator) {
+                    operator = value;
+                    isOperatorChosen = true;
+                } else {
+                    const output = operate(operator, parseFloat(num1), parseFloat(num2));
+                    resultDisplay.textContent = output; // Display the intermediate result
+                    num1 = output.toString();
+                    num2 = '';
+                    operator = value;
+                }
+            }
+        }
+    });
 });
 
-function operate(operator, num1, num2) {
-    let result;
-    if (operator == 'add') {
-        result = add(num1, num2);
-    } 
-    else if (operator == 'subtract') {
-        result = subtract(num1, num2);
-    } 
-    else if (operator == 'multiply') {
-        result = multiply(num1, num2);
-    } 
-    else {
-        result = divide(num1, num2);
+// ... [rest of the functions remain unchanged]
+
+// New function to toggle the sign
+function toggleSign(number) {
+    if (number.startsWith("-")) {
+        return number.slice(1);
+    } else {
+        return "-" + number;
     }
-    return result;
 }
 
-function add(a, b) {
-    return a + b;
+// New function to reset the calculator
+function resetCalculator() {
+    num1 = '';
+    num2 = '';
+    operator = null;
+    isOperatorChosen = false;
+    resultDisplay.textContent = '0';
 }
 
-function subtract(a, b) {
-    return a - b;
+
+function operate(op, a, b) {
+        switch (op) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "&times;":  // Using "X" based on your HTML for multiplication
+                return a * b;
+            case "/":
+                if (b === 0) {
+                    alert('Cannot divide by zero');
+                    return 'Error';
+                }
+                return a / b;
+            default:
+                return 'Error';
+        }
+    
 }
 
-function multiply(a, b) {
-    return a * b;
-}
 
-function divide(a, b) {
-    return a / b;
-}
 
 
 
